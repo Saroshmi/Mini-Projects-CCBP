d95188137d12344eb70899ce50a37714bc6bf463
Saroshmi B <121241857+Saroshmi@users.noreply.github.com>
2024-09-02 13:13:18 +0530

    Add files via upload

---
+++ Speed Typing Test/Speed Typing Test.js
@@ -0,0 +1,10 @@
+let timerEl = document.getElementById("timer");
+let quotedisplayEl = document.getElementById("quoteDisplay");
+let resultEl = document.getElementById("result");
+let quoteinputEl = document.getElementById("quoteInput");
+let submitbtnEl = document.getElementById("submitBtn");
+let resetbtnEl = document.getElementById("resetBtn");
+let spinnerEl = document.getElementById("spinner");
+let counter = 0;
+
+spinnerEl.classList.toggle("d-none");
